af5a10142407af936e1ee459e921d0376230f166
Brian <diazbrian47@gmail.com>
2024-02-11 21:04:41 -0500

    Creating project and adding maven dependencies

---
+++ Server/companerosDeViajeAPI/src/main/java/com/latam/companerosDeViajeAPI/CompanerosDeViajeApiApplication.java
@@ -0,0 +1,13 @@
+package com.latam.companerosDeViajeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanerosDeViajeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanerosDeViajeApiApplication.class, args);
+	}
+
+}
