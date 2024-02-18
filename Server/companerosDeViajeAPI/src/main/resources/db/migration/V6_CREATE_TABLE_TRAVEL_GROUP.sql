dfcd1baad9509eb33d9edf46f7edf209438fb295
Seujumon <jseura@gmail.com>
2024-02-18 14:52:51 -0300

    Add: migration for Travel Group table

---
+++ Server/companerosDeViajeAPI/src/main/resources/db/migration/V6_CREATE_TABLE_TRAVEL_GROUP.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `travel_group` (
+  `id` BIGINT PRIMARY KEY NOT NULL,
+  `budget` decimal(38,2) DEFAULT NULL,
+  `departure_date` datetime(6) DEFAULT NULL,
+  `destination` varchar(255) DEFAULT NULL,
+  `itinerary` varchar(255) DEFAULT NULL,
+  `minimum_number_of_members` int(11) DEFAULT NULL,
+  `return_date` datetime(6) DEFAULT NULL,
+  `owner_id` bigint(20) DEFAULT NULL
+);
