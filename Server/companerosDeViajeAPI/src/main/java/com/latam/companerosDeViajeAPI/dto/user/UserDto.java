1464fb7e5f30afd2cf1c2503619fd514d568f1c9
Brian <diazbrian47@gmail.com>
2024-02-20 19:52:50 -0500

    add birthDate into UserDto

--- Server/companerosDeViajeAPI/src/main/java/com/latam/companerosDeViajeAPI/dto/user/UserDto.java
+++ Server/companerosDeViajeAPI/src/main/java/com/latam/companerosDeViajeAPI/dto/user/UserDto.java
@@ -9,14 +9,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record UserDto(Long id, String name, String email, String phoneNumber,
-                      String address, Gender gender, String country, List<String> interest,
+                      String address, LocalDate birthDate, Gender gender, String country, List<String> interest,
                       String username, Role role) {
     public UserDto(User user, List<String> interest) {
         this(user.getId(), user.getName(),user.getEmail(),user.getPhoneNumber()
-                ,user.getAddress(),user.getGender(),
+                ,user.getAddress(),user.getBirthDate(),user.getGender(),
                 user.getCountry().getName(),interest,user.getUsername(),user.getRole());
     }
 }
